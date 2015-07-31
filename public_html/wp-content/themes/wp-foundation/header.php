1868b820cef9e8b602333afa0d49265c3abb2c64
Marc Guay <marc.guay@gmail.com>
2015-07-31 11:15:30 -0400

    Header tweaks

--- public_html/wp-content/themes/wp-foundation/header.php
+++ public_html/wp-content/themes/wp-foundation/header.php
@@ -4,11 +4,11 @@
  *
  */
 ?><!DOCTYPE html>
-<html <?php language_attributes(); ?>>
+<html <?php language_attributes(); ?> class="no-js">
 <head>
-	<title><?php wp_title(); ?></title>
+	
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 	<?php wp_head(); ?>
@@ -17,7 +17,6 @@
 <body <?php body_class(); ?>>
 <div id="page" class="hfeed site">
 
-	
 	<header id="masthead" class="site-header" role="banner">
 		<div class="site-branding">
 			<?php
