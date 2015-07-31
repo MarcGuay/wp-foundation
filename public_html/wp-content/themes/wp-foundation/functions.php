d6926f37016ecfa89947c442d769a44324c12388
Marc Guay <marc.guay@gmail.com>
2015-07-31 10:55:26 -0400

    Include scripts and styles

--- public_html/wp-content/themes/wp-foundation/functions.php
+++ public_html/wp-content/themes/wp-foundation/functions.php
@@ -4,8 +4,23 @@
  * Enqueue scripts and styles
  */
 function custom_assets() {
+	// JS
+	
+	// Header
+
+	wp_enqueue_script( 'modernizr', get_template_directory_uri() . '/bower_components/foundation/js/vendor/modernizr.js', array(), '2.8.3', false );
+	
+	// Footer 
+	
+	// Deregister the jquery version bundled with WordPress.
+	wp_deregister_script( 'jquery' );
+	wp_enqueue_script( 'jquery', get_template_directory_uri() . '/bower_components/foundation/js/vendor/jquery.js', array(), '2.1.4', true );	
+	
+	wp_enqueue_script( 'fastclick', get_template_directory_uri() . '/bower_components/foundation/js/vendor/fastclick.js', array(), '1.0.0', true );
+	wp_enqueue_script( 'app-js', get_template_directory_uri() . '/js/app-compiled.js', array('jquery'), '1.0.0', true );
+
+	// CSS
 	wp_enqueue_style( 'app-css', get_template_directory_uri() . '/css/app.css' );
-	wp_enqueue_script( 'app-js', get_template_directory_uri() . '/js/app.js', array(), '1.0.0', true );
 }
 
 add_action( 'wp_enqueue_scripts', 'custom_assets' );
