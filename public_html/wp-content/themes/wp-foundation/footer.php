9807960070c50a2f3d7e94fc73f213eff83f9b0f
Marc Guay <marc.guay@gmail.com>
2015-07-31 10:01:08 -0400

    Add some basic templates

---
+++ public_html/wp-content/themes/wp-foundation/footer.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ */
+?>
+
+	</div><!-- .site-content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+
+	</footer><!-- .site-footer -->
+
+</div><!-- .site -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
