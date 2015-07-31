9ad66210157653c273def0a03e3acfdc65d933be
Marc Guay <marc.guay@gmail.com>
2015-07-31 11:15:52 -0400

    Add basic index content from twentyfifteeen

--- public_html/wp-content/themes/wp-foundation/index.php
+++ public_html/wp-content/themes/wp-foundation/index.php
@@ -1,7 +1,27 @@
-<?php 
-get_header();
+<?php get_header(); ?>
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
 
+		<?php if ( have_posts() ) : ?>
 
-	//content
+			<?php if ( is_home() && ! is_front_page() ) : ?>
+				<header>
+					<h1><?php single_post_title(); ?></h1>
+				</header>
+			<?php endif; ?>
 
-get_footer();
+			<?php
+			while ( have_posts() ) : the_post();
+				get_template_part( 'content', get_post_format() );
+			endwhile;
+
+		// If no content, include the "No posts found" template.
+		else :
+			get_template_part( 'content', 'none' );
+		endif;
+		?>
+
+		</main><!-- .site-main -->
+	</div><!-- .content-area -->
+
+<?php get_footer(); ?>
