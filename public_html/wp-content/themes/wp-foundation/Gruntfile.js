0f7546acfa48f944f4538da9570089c607a0b261
Marc Guay <marc.guay@gmail.com>
2015-07-31 10:55:07 -0400

    Add JS concat and uglify to grunt script

--- public_html/wp-content/themes/wp-foundation/Gruntfile.js
+++ public_html/wp-content/themes/wp-foundation/Gruntfile.js
@@ -17,6 +17,55 @@ module.exports = function(grunt) {
       }
     },
 
+    concat: {
+        options: {
+          separator: ';',
+        },
+        dist: {
+          src: [
+
+          // Foundation core
+          'bower_components/foundation/js/foundation/foundation.js',
+
+          // Pick the componenets you need in your project
+          //'bower_components/foundation/js/foundation/foundation.abide.js',
+          //'bower_components/foundation/js/foundation/foundation.accordion.js',
+          //'bower_components/foundation/js/foundation/foundation.alert.js',
+          //'bower_components/foundation/js/foundation/foundation.clearing.js',
+          //'bower_components/foundation/js/foundation/foundation.dropdown.js',
+          //'bower_components/foundation/js/foundation/foundation.equalizer.js',
+          //'bower_components/foundation/js/foundation/foundation.interchange.js',
+          //'bower_components/foundation/js/foundation/foundation.joyride.js',
+          //'bower_components/foundation/js/foundation/foundation.magellan.js',
+          //'bower_components/foundation/js/foundation/foundation.offcanvas.js',
+          //'bower_components/foundation/js/foundation/foundation.orbit.js',
+          //'bower_components/foundation/js/foundation/foundation.reveal.js',
+          //'bower_components/foundation/js/foundation/foundation.slider.js',
+          //'bower_components/foundation/js/foundation/foundation.tab.js',
+          //'bower_components/foundation/js/foundation/foundation.tooltip.js',
+          'bower_components/foundation/js/foundation/foundation.topbar.js',
+
+          // app.js is included with foundation install and fires $(document).foundation();
+          'js/app.js',
+          
+          // Include your own custom scripts (located in the custom folder)
+          'js/custom/*.js'
+
+          ],
+          // Finally, concatinate all the files above into one single file
+          dest: 'js/app-compiled.js',
+        },
+    },
+
+    uglify: {
+      dist: {
+        files: {
+          // Shrink the file size by removing spaces
+          'js/app-compiled.js': ['js/app-compiled.js']
+        }
+      }
+    },
+    
     watch: {
       grunt: {
         options: {
@@ -28,13 +77,20 @@ module.exports = function(grunt) {
       sass: {
         files: 'scss/**/*.scss',
         tasks: ['sass']
-      }
+      },
+      
+      js: {
+          files: 'js/custom/**/*.js',
+          tasks: ['concat', 'uglify']
+      }      
     }
   });
 
   grunt.loadNpmTasks('grunt-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');  
 
-  grunt.registerTask('build', ['sass']);
+  grunt.registerTask('build', ['sass', 'concat', 'uglify']);
   grunt.registerTask('default', ['build','watch']);
 }
